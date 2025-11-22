de934aa1f142f08d1b1ab82d62fb6bd0f89cb815
Mahtab17 <chowdhury22205101076@diu.edu.bd>
2025-11-22 19:12:20 +0600

    tracking system js compleated successfully

--- resources/views/track.blade.php
+++ resources/views/track.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Track Feedback - VOBD</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&family=Noto+Sans+Bengali:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="track.css">
-    <link rel="stylesheet" href="tracking-styles.css">
+    <link rel="stylesheet" href="{{ asset('css/track.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/tracking-styles.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
@@ -348,7 +348,11 @@
         </div>
     </footer>
 
-    <script src="script.js"></script>
-    <script src="track.js"></script>
+     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="{{ asset('js/track.js') }}"></script>
+    <script src="{{ asset('js/tracking-app.js') }}"></script>
+
 </body>
 </html>
